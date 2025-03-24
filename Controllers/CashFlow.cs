e1e947a22c8fa61d1ef140baedb2145ce10ebab2
Abdullah <skabdullahabad01@gmail.com>
2025-03-24 13:15:20 +0530

    abdul

---
+++ Controllers/CashFlow.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Demo_Project_Game_Events.Controllers
+{
+    public class CashFlow : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
